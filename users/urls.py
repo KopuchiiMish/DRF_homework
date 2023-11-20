d7184058585228c45cf42d5f832ac543250bf825
nataliagovorushkina <a10061514@gmail.com>
2023-11-21 00:02:37 +0300

    26_2_3_

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from users.views import UserRetrieveAPIView, UserUpdateAPIView
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, UserCreateAPIView
 
 urlpatterns = [
+    path('register/', UserCreateAPIView.as_view(), name='user-create'),
     path('<int:pk>/', UserRetrieveAPIView.as_view(), name='user-detail'),
     path('<int:pk>/update/', UserUpdateAPIView.as_view(), name='user-update'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
